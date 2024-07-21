807eae4445bc894d2c782c0b1e47f345d8a1c858
Razzakh <mrazzakh123@gmail.com>
2024-07-21 18:43:38 +0530

    yes

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -10,8 +10,8 @@ const UseMemo = (props) => {
         {props.todo.map((item,i)=><p id={'todo-'+i} key={'a'+i}>{item}</p>)}
         <button id='add-todo-btn' onClick={()=>props.settodo(todo=>[...todo,'New Todo'])}>Add Todo</button>
         <hr/>
-        <p>Count:{props.count}
-            <button id='incr-cnt' onClick={()=>props.setcount(count+1)}>0</button>
+        <p id='incr-cnt'>Count:{props.count}
+            <button id='incr-btn' onClick={()=>props.setcount(count=>count+1)}>0</button>
         </p>
     </div>
   )
